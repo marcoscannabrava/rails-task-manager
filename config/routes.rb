e16a37a6bc12aba2577cb7380a916311a3e1f5c8
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-11-21 07:02:25 -0300

    fixed controllers and updated readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root                     to: 'tasks#list'
+  get    'tasks',          to: 'tasks#list'
+  get    'tasks/new',      to: 'tasks#add'
+  post   'tasks',          to: 'tasks#create'
+  # NB: The `show` route needs to be *after* `new` route.
+  get    'tasks/:id',      to: 'tasks#edit', as: :task
+  patch  'tasks/:id',      to: 'tasks#update'
+  delete 'tasks/:id',      to: 'tasks#delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
